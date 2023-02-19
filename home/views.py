7673409164db50b37ab1001dd6eb6ad9a3f8a247
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-20 00:17:49 +0330

    add home app

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.views.generic import View
+
+class HomePageView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/home.html')
+
+
