e61e492d162db81dfaa18118318130fd8395ed6d
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-24 01:47:07 +0330

    write validation for user register serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.core.validators import MinLengthValidator
+
+from django.contrib.auth.models import User
 
 
 class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    username = serializers.CharField(validators=[
+        UniqueValidator(queryset=User.objects.all()),
+        MinLengthValidator(3, 'username must be at least 3 characters.')
+    ])
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError('password confirm mismatch.')
+        return attrs
