04f407d2fa9d5ef32c229b9146a9f9135e2569f3
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-23 18:13:53 +0330

    add functionality for registering a user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+
+
+class UserRegister(APIView):
+    def post(self, request):
+        ser_data = UserRegisterSerializer(data=request.data)
+        if ser_data.is_valid():
+            User.objects.create_user(
+                username=ser_data.validated_data['username'],
+                email=ser_data.validated_data['email'],
+                password=ser_data.validated_data['password']
+            )
+            return Response(ser_data.data)
+        return Response(ser_data.errors)
