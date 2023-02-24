cb79fb2617917551923f5859633b468d94e8e91c
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-24 22:59:45 +0330

    add some permission classes to home view

--- home/views.py
+++ home/views.py
@@ -1,22 +1,15 @@
-# from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Person
 from .serializers import PersonSerializer
 
-# @api_view()
-# def home(request):
-#     return Response({
-#         'NID': '0440687373',
-#         'name': "Hossein",
-#         'age': 25
-#     })
 
 class Home(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
     def get(self, request):
         persons = Person.objects.all()
         ser_data = PersonSerializer(persons, many=True)
         return Response(ser_data.data)
-
-
