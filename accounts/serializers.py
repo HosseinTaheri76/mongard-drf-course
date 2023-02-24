3ec19c21104357059f8c5163d68486fd84749e48
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-24 12:00:09 +0330

    change user register serializer to model serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 from django.core.validators import MinLengthValidator
 
 from django.contrib.auth.models import User
 
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(validators=[
-        UniqueValidator(queryset=User.objects.all()),
-        MinLengthValidator(3, 'username must be at least 3 characters.')
-    ])
-    email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
+
     password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username': {'validators': [MinLengthValidator(3, 'username must be at least 3 characters.')]}
+        }
+
+    def validate_username(self, value):
+        if 'admin' in value:
+            raise serializers.ValidationError('using admin as username is prohibited.')
+        return value
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
