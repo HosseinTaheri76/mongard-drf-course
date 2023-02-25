1f08d5dcd769856d49f63f96b2b95c3c021d38fb
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-25 19:41:48 +0330

    add functionality for GET ing questions

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
 
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Question, Answer
+from .serializers import PersonSerializer, QuestionSerializer, AnswerSerializer
 
 
 class Home(APIView):
@@ -13,3 +14,9 @@ class Home(APIView):
         persons = Person.objects.all()
         ser_data = PersonSerializer(persons, many=True)
         return Response(ser_data.data)
+
+class QuestionView(APIView):
+
+    def get(self, request):
+        srz_data = QuestionSerializer(Question.objects.all(), many=True).data
+        return Response(srz_data)
