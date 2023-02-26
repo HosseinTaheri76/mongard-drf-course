2071425a6d2c67ce3debf76b63c3d4f2561f2468
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-26 21:00:23 +0330

    add post, put, delete

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('questions/', views.QuestionView.as_view(), name='questions'),
+    path('questions/<int:pk>/', views.QuestionView.as_view(), name='question_rud'),
 ]
--- home/views.py
+++ home/views.py
@@ -20,3 +20,23 @@ class QuestionView(APIView):
     def get(self, request):
         srz_data = QuestionSerializer(Question.objects.all(), many=True).data
         return Response(srz_data)
+
+    def post(self, request):
+        srz_data = QuestionSerializer(data=request.data)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_201_CREATED)
+        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        srz_data = QuestionSerializer(instance=question, data=request.data, partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_200_OK)
+        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        question.delete()
+        return Response({'message': 'question deleted'}, status=status.HTTP_200_OK)
